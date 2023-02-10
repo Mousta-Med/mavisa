508f7b919a02276fb7441600404f559f20a51bfe
Mohammed Moustarhfir <112891663+Mousta-Med@users.noreply.github.com>
2023-02-10 19:30:22 +0100

    commit

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -7,11 +7,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+    <link rel="stylesheet" type="text/css" href="/MaVisa/public/css/style.css" />
 </head>
 
 <body>
-
-    <div id="app">{{ name }}</div>
+    <div class="container-fluid">
+        <div class="home">
+            <nav class="navbar navbar-expand-sm navbar-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="">Mavisa</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="mynavbar">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="home">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="rooms">Rooms</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class="headerinfo ">
+                <p>hfjkfgdsjgfdsh d qfhgkfjdgsqf dqfdgjfsdhgfjdqsjgjdsgfj fqs dsghjf
+                </p>
+            </div>
+        </div>
+    </div>
 
     <script src="/mavisa/public/js/script.js"></script>
 </body>
